89f07bf52dd1cad0fb5caa4c8b21cab1d2faa6bc
Jon R. Roma <jonroma@jonroma.net>
2022-05-04 15:29:17 -0500

    Be DRY; use terraform-aws-util//modules/get-subnets

--- data.tf
+++ data.tf
@@ -1,35 +1,28 @@
+# FIXME: Replace reference to branch with reference to tagged version.
+
+module "get-subnets" {
+  source = "github.com/techservicesillinois/terraform-aws-util//modules/get-subnets?ref=v3.0.4"
+
+  count = local.subnet_type != "" ? 1 : 0
+
+  subnet_type = local.subnet_type
+  vpc         = local.vpc
+}
+
 locals {
-  tier    = lookup(var.network_configuration, "tier", "")
-  vpc     = lookup(var.network_configuration, "vpc", "")
-  subnets = compact(split(" ", lookup(var.network_configuration, "subnets", "")))
+  subnet_ids = flatten([for v in module.get-subnets : v.subnets.ids])
+  vpc_id     = element(distinct([for v in module.get-subnets : v.vpc.id]), 0)
+}
 
-  all_subnets = distinct(
-    concat(flatten(data.aws_subnet_ids.selected.*.ids), local.subnets),
-  )
-
-  nc_security_group_names = compact(
-    split(" ", lookup(var.network_configuration, "security_group_names", "")),
-  )
+locals {
+  subnets                 = compact(split(" ", lookup(var.network_configuration, "subnets", "")))
+  subnet_type             = lookup(var.network_configuration, "subnet_type", "")
+  vpc                     = lookup(var.network_configuration, "vpc", "")
+  all_subnets             = distinct(concat(local.subnet_ids, local.subnets))
+  nc_security_group_names = compact(split(" ", lookup(var.network_configuration, "security_group_names", "")))
 }
 
 data "aws_security_group" "selected" {
   count = length(local.nc_security_group_names)
   name  = local.nc_security_group_names[count.index]
-}
-
-data "aws_vpc" "vpc" {
-  count = local.tier != "" ? 1 : 0
-
-  tags = {
-    Name = local.vpc
-  }
-}
-
-data "aws_subnet_ids" "selected" {
-  count  = local.tier != "" ? 1 : 0
-  vpc_id = data.aws_vpc.vpc[0].id
-
-  tags = {
-    Tier = local.tier
-  }
 }
--- variables.tf
+++ variables.tf
@@ -2,6 +2,11 @@ variable "name" {
   description = "REQUIRED: The name of the cluster. Should reflect the service utilizing redis."
 }
 
+variable "description" {
+  description = "A description of the cluster."
+  default     = ""
+}
+
 variable "redis_clusters" {
   description = "Number of Redis cache clusters (nodes) to create"
   default     = 2
@@ -44,6 +49,13 @@ variable "redis_snapshot_retention_limit" {
   description = "The number of days for which ElastiCache will retain automatic cache cluster snapshots before deleting them. For example, if you set SnapshotRetentionLimit to 5, then a snapshot that was taken today will be retained for 5 days before being deleted. If the value of SnapshotRetentionLimit is set to zero (0), backups are turned off. Please note that setting a snapshot_retention_limit is not supported on cache.t1.micro or cache.t2.* cache nodes"
   default     = 0
 }
+
+variable "tags" {
+  description = "Tags to be applied to resources where supported"
+  type        = map(string)
+  default     = {}
+}
+
 
 ### NETWORK CONFIGURATION block
 variable "network_configuration" {
